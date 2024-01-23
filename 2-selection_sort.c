c696e4a11324fda9c764b544a2d23404d43cbb90
KiPTM <mathewtuwei@gmail.com>
2024-01-23 07:10:58 -0800

    betty sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -11,29 +11,26 @@ void selection_sort(int *arr, size_t size)
 {
 	size_t i, j;
 	int temp;
-	
+
 	if (arr == NULL || size < 2)
-        return;
-	
+		return;
 	for (i = 0; i < size - 1; i++)
 	{
-        size_t min_index = i;
+		size_t min_index = i;
 
-        for (j = i + 1; j < size; j++)
-        {
-            if (arr[j] < arr[min_index])
-                min_index = j;
-        }
-
-        if (i != min_index)
-        {
-            /* Swap elements */
-            temp = arr[i];
-            arr[i] = arr[min_index];
-            arr[min_index] = temp;
-
-            /* Print the array after each swap */
-            print_array(arr, size);
-        }
+		for (j = i + 1; j < size; j++)
+		{
+			if (arr[j] < arr[min_index])
+				min_index = j;
+		}
+		if (i != min_index)
+		{
+			/* Swap elements */
+			temp = arr[i];
+			arr[i] = arr[min_index];
+			arr[min_index] = temp;
+			/* Print the array after each swap */
+			print_array(arr, size);
+		}
 	}
 }
