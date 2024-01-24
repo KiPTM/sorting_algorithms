b2c944d64b051b868f06bef5c744a8af6ec1b650
KiPTM <mathewtuwei@gmail.com>
2024-01-24 03:40:31 -0800

    Betty sort Insertion

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -9,33 +9,28 @@ void insertion_sort_list(listint_t **list)
 {
 	listint_t *current = (*list)->next;
 	listint_t *temp;
-	
+
 	if (list == NULL || *list == NULL || (*list)->next == NULL)
-        return;
-	
+		return;
 	while (current != NULL)
-    {
-        temp = current;
-        while (temp->prev != NULL && temp->n < temp->prev->n)
-        {
-            /* Swap nodes */
-            temp->prev->next = temp->next;
-            if (temp->next != NULL)
-                temp->next->prev = temp->prev;
-
-            temp->next = temp->prev;
-            temp->prev = temp->next->prev;
-            temp->next->prev = temp;
-
-            if (temp->prev != NULL)
-                temp->prev->next = temp;
-            else
-                *list = temp;
-
-            /* Print the list after each swap */
-            print_list(*list);
-        }
-
-        current = current->next;
-    }
+	{
+		temp = current;
+		while (temp->prev != NULL && temp->n < temp->prev->n)
+		{
+			/* Swap nodes */
+			temp->prev->next = temp->next;
+			if (temp->next != NULL)
+				temp->next->prev = temp->prev;
+			temp->next = temp->prev;
+			temp->prev = temp->next->prev;
+			temp->next->prev = temp;
+			if (temp->prev != NULL)
+				temp->prev->next = temp;
+			else
+				*list = temp;
+			/* Print the list after each swap */
+			print_list(*list);
+		}
+		current = current->next;
+	}
 }
